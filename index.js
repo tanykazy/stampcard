046ba0ff0f231707d501d6d67268c333eb274cb3
tanykazy <tanykazy@gmail.com>
2025-07-24 00:11:25 +0900

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,13 @@ var app = express();
 
 app.use(express.static("dist/stampcard"));
 
-app.get('/', function (req, res) {
-    res.redirect('/');
-});
+// app.get('/', function (req, res) {
+//     res.redirect('/');
+// });
 
-app.listen(8080);
+// app.listen(8080);
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
